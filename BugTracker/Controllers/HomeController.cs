ec656980d66a19eb85f62004bc3e08587ec663c0
ezhong0 <edwardrzhong@gmail.com>
2022-02-21 22:18:47 -0800

    readme

--- BugTracker/Controllers/HomeController.cs
+++ BugTracker/Controllers/HomeController.cs
@@ -259,8 +259,8 @@ namespace BugTracker.Controllers
                 da.SelectCommand = new MySqlCommand(sql, conn);
                 da.Fill(ds);
             }
-
-            ds.DefaultView.Sort = "title desc";
+			
+			ds.DefaultView.Sort = "title desc";
 
             ViewBag.projects = (from DataRow dr in ds.Rows
                                 select new DashboardModel()
@@ -273,6 +273,26 @@ namespace BugTracker.Controllers
                                     UserId = Convert.ToInt32(dr["userid"])
 
                                 }).ToList();
+
+            //ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            //ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+            //var students = from s in db.Students
+            //               select s;
+            //switch (sortOrder)
+            //{
+            //    case "name_desc":
+            //        students = students.OrderByDescending(s => s.LastName);
+            //        break;
+            //    case "Date":
+            //        students = students.OrderBy(s => s.EnrollmentDate);
+            //        break;
+            //    case "date_desc":
+            //        students = students.OrderByDescending(s => s.EnrollmentDate);
+            //        break;
+            //    default:
+            //        students = students.OrderBy(s => s.LastName);
+            //        break;
+            //}
 
         }
 
