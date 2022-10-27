f95e6df154cef917ed301554a35450b30d46eba4
ezhong0 <edwardrzhong@gmail.com>
2022-10-26 22:37:27 -0700

    php bugfix

--- BugTracker/Views/php/login.php
+++ BugTracker/Views/php/login.php
@@ -2,18 +2,18 @@
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-if(!empty($username) || !empty($password) {
+if(!empty($username) || !empty($password)) {
     $host = "localhost";
-    $dbUsername = "root"
-    $dbPassword = "Bl@ckpink"
-    $dbname = "bugtrackerdb"
+    $dbUsername = "root";
+    $dbPassword = "Bl@ckpink";
+    $dbname = "bugtrackerdb";
 
     $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
 
     if(mysqli_connect_error()) {
         die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
     } else {
-        $SELECT = "select USERNAME from USER where USERNAME = ? and PASSWORD = ? Limit 1"
+        $SELECT = "select USERNAME from USER where USERNAME = ? and PASSWORD = ? Limit 1";
         $stmt = $conn->prepare($SELECT);
         $stmt->bind_param("ss", $username, $password);
         $stmt->execute();
