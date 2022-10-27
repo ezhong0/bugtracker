7c42db176608dd49c1d3166af60fa992f35927f4
ezhong0 <edwardrzhong@gmail.com>
2022-10-26 23:24:54 -0700

    revert

--- BugTracker/Views/Models/UserViewModel.cs
+++ BugTracker/Views/Models/UserViewModel.cs
@@ -24,7 +24,7 @@ namespace BugTracker.Views.Models
         {
             var path = $"Users/{id}";
             client = new HttpClient();
-            client.BaseAddress = new Uri("/api/");
+            client.BaseAddress = new Uri("https://localhost:5001/api/");
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(
                 new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
